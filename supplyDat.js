3d8eeac4cd1411b7312cf5128c4c51644df58386
sevakd <sevak.gd@gmail.com>
2012-07-06 17:47:52 -0400

    changed title attr to impact fact

--- supplyDat.js
+++ supplyDat.js
@@ -1,6 +1,6 @@
 function fetchSupplyDat(){
-    datAtt = "id: " +dat.supplychain.id +'\n';
-    datAtt+= "title: " +dat.supplychain.stops[1].attributes.co2e;
+    datAtt = "id: " +dat.supplychain.id +'\n| ';
+    datAtt+= "impact factor(co2e): " +dat.supplychain.stops[1].attributes.co2e;
     document.getElementById('dat').innerHTML=datAtt;
     
     var dataset=JSON.stringify(dat);    
