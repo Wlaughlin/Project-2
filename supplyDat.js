4090598af9cb84f51746dcaa0d0ee6e871cc0ddd
sevakd <sevak.gd@gmail.com>
2012-07-06 17:28:08 -0400

    added simple d3 vis for json obj; appends paragraph for every val found

--- supplyDat.js
+++ supplyDat.js
@@ -2,4 +2,11 @@ function fetchSupplyDat(){
     datAtt = "id: " +dat.supplychain.id +'\n';
     datAtt+= "title: " +dat.supplychain.stops[1].attributes.co2e;
     document.getElementById('dat').innerHTML=datAtt;
+    
+    var dataset=JSON.stringify(dat);    
+    d3.select("body").selectAll("p")
+        .data(dataset)
+        .enter()
+        .append("p")
+        .text("found val in json obj!");
 }
