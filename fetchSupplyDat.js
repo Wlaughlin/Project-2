0641b9f19f09a1793b9b910a051f31e80b2e8403
sevakd <sevak.gd@gmail.com>
2012-07-10 10:31:43 -0400

    added comments

--- fetchSupplyDat.js
+++ fetchSupplyDat.js
@@ -1,4 +1,6 @@
 function ajaxRequest(){
+    
+    //create xmlHttpReq object
     var xmlhttp;
     if (window.XMLHttpRequest){
         xmlhttp=new XMLHttpRequest();
@@ -6,18 +8,25 @@ function ajaxRequest(){
 
     //recieve data sent from sever
     xmlhttp.onreadystatechange=function(){
+
+        //ready?
         if(xmlhttp.readyState == 4){
+            //pass response to supVis
             supVis(JSON.parse(xmlhttp.responseText));
         }   
     }
-
+    
+    //get form input value
     var numS = encodeURIComponent(document.getElementById("num").value);
 
+    //send to value  php
     xmlhttp.open("POST","jsonLoad.php?num=" +numS,true);
     xmlhttp.send(null);
 }
 
 function supVis(dat){
+    
+    //visualize data
     datAtt = dat.supplychain.id;
     document.getElementById('dat').innerHTML=datAtt;
 }
