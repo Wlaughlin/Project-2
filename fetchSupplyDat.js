b9943f72242c60e24d3237e6af0842c5604bd0c4
sevakd <sevak.gd@gmail.com>
2012-07-19 16:12:13 -0400

    added links

--- fetchSupplyDat.js
+++ fetchSupplyDat.js
@@ -50,21 +50,19 @@ function supVis(dat){ //visuailize data
         for(var i = 0; i<h.length; ++i){ //get links
             from = h[i].from_stop_id; //source
             to = h[i].to_stop_id; //target
-            //stop ids are in descending order
-            //thus correct stop = num of stops - stop id
-            links.push({"source" : s[s.length-from].attributes.title, "target" : s[s.length-to].attributes.title});
+            //node index locations
+            links.push({"source" : s[s.length-from].id, "target" : s[s.length-to].id});
         }
     }
-
    if(typeof(s) != 'undefined'){
         for(var i = 0; i<s.length; ++i){ //get nodes
             nodes.push({"name" : s[i].attributes.title});
         }
     }
 
-    var w = 500, //set width and height
-        h = 100;
-
+    var w = 800, //set width and height
+        h = 400;
+    
     var sLoc = [];
     var s2Loc = [];
 
@@ -80,12 +78,24 @@ function supVis(dat){ //visuailize data
         .append("circle")
         .attr("cx", function(){
             xLoc = Math.random() * w;
-            sLoc.push(xLoc);
+            if(xLoc > w-50){
+                xLoc = xLoc - 20;
+            }
+            else if(xLoc < 50){
+                xLoc = xLoc + 20;
+            }
+            sLoc.push(xLoc); //save cx values
             return xLoc;
         })
         .attr("cy", function(){
             yLoc = Math.random() * h;
-            s2Loc.push(yLoc);
+            if(yLoc > h-50){
+                yLoc = yLoc - 20;
+            }
+            else if(yLoc < 50){
+                yLoc = yLoc + 20;
+            }
+            s2Loc.push(yLoc); //save cy values
             return yLoc;
         })
         .attr("r", 5);
@@ -101,6 +111,27 @@ function supVis(dat){ //visuailize data
             return sLoc[i]
         })
         .attr("y", function(d, i){
-            return s2Loc[i] - 20
+            return s2Loc[i] - 10
         });
+    console.log(nodes);
+    console.log(links);
+    console.log(sLoc);
+    console.log(s2Loc);
+    svg.selectAll("line") //line for each link
+        .data(links)
+        .enter()
+        .append("line")
+        .attr("x1", function(d){
+            return sLoc[d.source-1];
+        })
+        .attr("y1", function(d){
+            return s2Loc[d.source-1];
+        })
+        .attr("x2", function(d){
+            return sLoc[d.target-1];
+        })
+        .attr("y2", function(d){
+            return s2Loc[d.target-1];
+        })
+        .attr("style", "stroke:rgb(0,0,0);stroke-width:2")
 }
